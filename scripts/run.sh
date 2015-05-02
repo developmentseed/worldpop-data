16e6078b8886b14ef9380f032cad078edc33e217
Gregor MacLennan <gmaclennan@digital-democracy.org>
2015-05-02 12:47:17 -0700

    Fix run.sh and add mapnik dependency

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+mkdir -p temp
 mkdir -p shapes
 mkdir -p tiles
 
@@ -8,7 +9,7 @@ for i in "$@" ; do scripts/vectorize.sh $i 1 density shapes ; done
 scripts/merge.sh shapes shapes/population.shp
 scripts/merge.sh shapes/coverage shapes/coverage.shp
 
-mapnik-shapeindex.js -d 12 shapes/population.shp
+node_modules/.bin/mapnik-shapeindex.js -d 12 shapes/population.shp
 
 scripts/tiles.js shapes/population.shp tiles/population.mbtiles population 10 12
 scripts/tiles.js shapes/coverage.shp tiles/coverage.mbtiles coverage 0 22
