c232f9855693f477b431bcc2529a2fd849808808
Anand Thakker <vestibule@anandthakker.net>
2015-05-19 09:40:20 -0400

    simplification for polygon features

--- scripts/simplify.sql
+++ scripts/simplify.sql
@@ -1,10 +1,25 @@
-DROP TABLE coverage_001;
-CREATE TABLE coverage_001 () WITH ( OIDS=FALSE );
-SELECT AddGeometryColumn('public', 'coverage_001', 'wkb_geometry', 4326, 'POLYGON', 2);
+DROP TABLE coverage_005;
+CREATE TABLE coverage_005 () WITH ( OIDS=FALSE );
+SELECT AddGeometryColumn('public', 'coverage_005', 'wkb_geometry', 4326, 'POLYGON', 2);
 
-INSERT INTO coverage_001
-  (SELECT ST_AsEWKB(ST_Simplify(ST_GeomFromEWKB(wkb_geometry), 0.01))
+INSERT INTO coverage_005
+  (SELECT ST_AsEWKB(ST_Simplify(ST_GeomFromEWKB(wkb_geometry), 0.05))
     as wkb_geometry FROM coverage);
 
-CREATE INDEX coverage_001_wkb_geometry_geom_idx
-  ON public.coverage_001 USING gist (wkb_geometry);
+CREATE INDEX coverage_01_wkb_geometry_geom_idx
+  ON public.coverage_005 USING gist (wkb_geometry);
+
+
+DROP TABLE population_005;
+CREATE TABLE population_005 (density numeric(10)) WITH ( OIDS=FALSE );
+SELECT AddGeometryColumn('public', 'population_005', 'wkb_geometry', 4326, 'POLYGON', 2);
+
+INSERT INTO population_005
+(SELECT
+  density,
+  ST_AsEWKB(ST_Simplify(ST_GeomFromEWKB(wkb_geometry), 0.05)) as wkb_geometry
+  FROM population);
+
+CREATE INDEX population_005_wkb_geometry_geom_idx
+  ON public.population_005 USING gist (wkb_geometry);
+
