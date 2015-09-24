583b1c7d10224bd2ac837072a298f0eb4f461322
Anand Thakker <vestibule@anandthakker.net>
2015-09-24 18:45:24 -0400

    Update 7z extraction script

--- scripts/pluck.js
+++ scripts/pluck.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env iojs --harmony_arrow_functions
+#!/usr/bin/env node --harmony_arrow_functions
 
 var fs = require('fs')
 var path = require('path')
@@ -7,58 +7,66 @@ var exec = require('child_process').exec
 var files = process.argv.slice(2)
 
 var dryrun = false
-if (files[0] === '--dryrun') {
-  dryrun = true
-  files.shift()
-}
 
 dofile()
 
-function dofile () {
+function dofile (err) {
+  if (err) {
+    console.error(files[0], err)
+  }
   var file = files.shift()
-  if (!file) return
+  if (!file) { return }
+  console.log(path.basename(file))
 
-  process.stdout.write('\n' + path.basename(file))
   var code = path.basename(file).split('-')[0]
 
-  if (fs.existsSync(file)) {
-    exec(
-      '7z l ' + file + '', (err, stdout, stderr) => {
-        if (err) {
-          process.stdout.write('\tskipping, error listing archive: ' + JSON.stringify(err))
-          return dofile()
-        }
+  console.log('\tdownloading')
+  exec('aws s3 cp s3://world-pop/' + file + ' .', function (err, stdout, stderr) {
+    if (err) { return dofile(err) }
+    exec('7z l ' + file + '', (err, stdout, stderr) => {
+      if (err) { return dofile(err) }
 
-        var allFiles = stdout.toString()
-          .match(RegExp(code + '[^ \t\n]+', 'g'))
+      var allFiles = stdout.toString().match(RegExp(code + '[^ \t\n]+', 'g'))
 
-        var toExtract = allFiles
-        .map((f) => f.match(/.*([0-9]+).*adj.*\.tif$/))
-        .filter((match) => (match !== null && match.length > 0))
-        .sort((a, b) => Number(b[1]) - Number(a[1]))
-        .map((match) => match[0])
+      var toExtract = allFiles
+      .map(f => f.match(/.*([0-9]+).*adj.*\.tif$/))
+      .filter(match => (match !== null && match.length > 0))
+      .sort((a, b) => +b[1] - +a[1])
+      .map(match => match[0])
 
-        if (toExtract.length === 0) {
-          process.stdout.write('\tskipping, no suitable TIFs: ')
-          process.stdout.write(allFiles.join(','))
-          return dofile()
-        } else if (toExtract.length > 1) {
-          var pph = toExtract.filter((file) => /pph/.test(file))
-          if (pph.length > 0) toExtract = pph
-        }
-
-        // we got a single desired file, so extract it.
-        process.stdout.write('\textracting ' + toExtract[0])
-
-        if (dryrun) return dofile()
-
-        exec('7z x -bd -y ' + file + ' ' + toExtract[0] + '*', (err, stdout, stderr) => {
-          if (err) console.error(err)
-          else process.stdout.write('\tcomplete\n')
-          dofile()
-          // fs.unlink(file, function () { process.stdout.write('deleted ' + file) })
-        })
+      if (toExtract.length === 0) {
+        return dofile(new Error('skipping, no suitable TIFs: ' + allFiles.join(',')))
+      } else if (toExtract.length > 1) {
+        var pph = toExtract.filter((file) => /pph/.test(file))
+        if (pph.length > 0) toExtract = pph
       }
-    )
-  }
+
+      console.log('\textracting ' + toExtract[0])
+
+      if (dryrun) return dofile()
+
+      exec('7z x -bd -y ' + file + ' ' + toExtract[0], (err, stdout, stderr) => {
+        if (err) {
+          return dofile(err)
+        } else {
+          console.log('\tuploading ' + toExtract[0])
+          exec('gzip ' + toExtract[0], function (err) {
+            if (err) { dofile(err) }
+            var ready = toExtract[0] + '.gz'
+            exec('aws s3 cp ' + ready + ' s3://world-pop/extracted/', (err, stdout, stderr) => {
+              if (err) { console.error(file, err) }
+              fs.unlink(file, function (err) {
+                if (err) { return dofile(err) }
+                fs.unlink(ready, function (err) {
+                  if (err) { return dofile(err) }
+                  console.log('deleted ' + file + ' and ' + ready)
+                  dofile()
+                })
+              })
+            })
+          })
+        }
+      })
+    })
+  })
 }
